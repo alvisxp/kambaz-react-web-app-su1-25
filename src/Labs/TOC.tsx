ede9685f550c4dc596dd16367611f127ee1921c7
alvisxp <128601501+alvisxp@users.noreply.github.com>
2025-05-23 14:10:35 -0400

    Lab 3 done

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,31 +1,21 @@
-// import {Link} from "react-router";
-// export default function TOC() {
-//     return (
-//         <ul>
-//             <li><Link to="/Labs">Labs</Link></li>
-//             <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-//             <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-//             <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-//             <li><Link to="/Kambaz">Kambaz</Link></li>
-//         </ul>
-//     );
-// }
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
 export default function TOC() {
- return (
-   <Nav variant="pills">
-     <Nav.Item>
+  const {pathname} = useLocation();
+  return (
+   <Nav variant="pills" id = "wd-toc">
+     {/* <Nav.Item>
        <Nav.Link to="/Labs" as={Link}>Lab 1</Nav.Link>
+     </Nav.Item> */}
+     <Nav.Item>
+       <Nav.Link to="/Labs/Lab1" as={Link} id="wd-a1" active ={pathname.includes('Lab1')}>Lab 1</Nav.Link>
      </Nav.Item>
      <Nav.Item>
-       <Nav.Link to="/Labs/Lab1" as={Link}>Lab 1</Nav.Link>
+       <Nav.Link to="/Labs/Lab2" as={Link} id="wd-a2" active ={pathname.includes('Lab2')}>Lab 2</Nav.Link>
      </Nav.Item>
      <Nav.Item>
-       <Nav.Link to="/Labs/Lab2" as={Link} active>Lab 2</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link to="/Labs/Lab3" as={Link}>Lab 3</Nav.Link>
+       <Nav.Link to="/Labs/Lab3" as={Link} id="wd-a3" active ={pathname.includes('Lab3')}>Lab 3</Nav.Link>
      </Nav.Item>
      <Nav.Item>
        <Nav.Link to="/Kambaz" as={Link}>Kambaz</Nav.Link>
